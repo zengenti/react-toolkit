1647d28e08e356a9b43eec6f5b33b4d3a82328b6
ZenSamClifford <s.clifford@zengenti.com>
2022-09-02 15:45:16 +0100

    fix: isSSR test

--- src/utilities/isSsr/isSsr.test.ts
+++ src/utilities/isSsr/isSsr.test.ts
@@ -1,12 +1,12 @@
 import isSsr from "./index";
 
 test('isSsr true', () => {
-  expect(isSsr()).toBe(true);
+  expect(isSsr()).toBe(false);
 });
 
 test('isSsr false', () => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   delete global.window;
-  expect(isSsr()).toBe(false);
+  expect(isSsr()).toBe(true);
 });
